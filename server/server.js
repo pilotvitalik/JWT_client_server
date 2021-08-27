77202798912a17936aa25ba4b8d980e88eb146f3
Vitaliy <12345678t>
2021-08-27 15:44:54 +0300

    - Create router on server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const router = require('./router');
 
 const PORT = process.env.PORT || 7000;
 const app = express();
+
+app.use(express.json);
+app.use(cookieParser);
+app.use('/api', router);
 
 const start = async () => {
     try {
