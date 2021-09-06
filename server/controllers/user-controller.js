c565280a658c7279e2d4f0b1053fca5d6e9e9f44
Vitaliy <itvkip@yandex.ru>
2021-09-06 22:07:40 +0300

    Udate token functionality

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -24,7 +24,6 @@ class UserController {
             const userData = await userService.login(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
             return res.json(userData);
-
         } catch (err){
             next(err);
         }
@@ -32,7 +31,10 @@ class UserController {
 
     async logout(req, res, next){
         try{
-
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken);
+            res.clearCookie('refreshToken');
+            return res.json(token);
         } catch (err){
             next(err);
         }
@@ -50,7 +52,10 @@ class UserController {
 
     async refresh(req, res, next){
         try{
-
+            const {refreshToken} = req.cookies;
+            const userData = await userService.refresh(refreshToken);
+            res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true})
+            return res.json(userData);
         } catch (err){
             next(err);
         }
