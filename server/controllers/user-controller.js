808857e93aa70563efb437df0892e4fab73397e5
Vitaliy <itvkip@yandex.ru>
2021-09-06 21:34:31 +0300

    Set validation body reuest

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,10 +1,14 @@
 const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
 const ApiError = require('../exception/api-error');
 
 class UserController {
     async registration(req, res, next){
         try{
-            //res.json([200, 204]);
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return next(ApiError.BadRequest('Ошибка при валидации', errors.array()))
+            }
             const {email, password} = req.body;
             const userData = await userService.registartion(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
